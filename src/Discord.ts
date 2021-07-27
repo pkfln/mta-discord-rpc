e6946d63ca21f8a086efc73688990e3e8e7e0410
BallTH9 <68109093+BallTH9@users.noreply.github.com>
2021-07-27 22:00:01 +0700

    Update Discord.ts

--- src/Discord.ts
+++ src/Discord.ts
@@ -4,7 +4,7 @@ import got from 'got';
 import { IQueryResponse } from './MTAQuery';
 
 const DEFAULT_IMAGE_KEY = 'mtasa';
-const clientId = '869499599549767720';
+const clientId = '794312139908317235';
 
 class Discord {
   private rpcClient: DiscordRPC.Client;
